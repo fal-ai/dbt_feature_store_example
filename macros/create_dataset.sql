4e35791ddcc811c6a3c9c11c3d62a3b9f85a1e26
Matteo Ferrando <matteo.ferrando2@gmail.com>
2022-02-08 18:53:39 -0400

    small refactor of create_dataset

--- macros/create_dataset.sql
+++ macros/create_dataset.sql
@@ -4,48 +4,49 @@
         label_name
     ) %}
 {% set feature_columns = ["trip_count_last_week", "trip_duration_last_week"] %}
-{% set ns = namespace(
-    label_table_timestamp_column = "",
-    label_table_entity_id_column = ""
+{% set label_table = namespace(
+    timestamp_column = "",
+    entity_id_column = ""
 ) %}
 {% if execute %}
-    {% set node = graph.nodes["model.my_new_project." + label_table_model] %}
-    {% do log(node, info=true) %}
-    {% set ns.label_table_timestamp_column = node.config.meta.fal.feature_store.timestamp %}
-    {% set ns.label_table_entity_id_column = node.config.meta.fal.feature_store.entity_id %}
 
-    SELECT
-        lb.{{ ns.label_table_entity_id_column }},
-        lb.{{ ns.label_table_timestamp_column }},
-        {% for column in feature_columns %}
-        {{ column }},
-        {% endfor %}
-        lb.{{ label_name }}
-    FROM
-        {{ ref(label_table_model) }} AS lb
+{% set node = graph.nodes["model.bike." + label_table_model] %}
+{% set label_table.timestamp_column = node.config.meta.fal.feature_store.timestamp %}
+{% set label_table.entity_id_column = node.config.meta.fal.feature_store.entity_id %}
 
-        {% for group in features %}
-        {{ stage_table_join(
-            "stg_" + group.feature_model_name + "_" + label_table_model,
-            "date",
-            "bike_id",
-            label_name,
-            ns.label_table_timestamp_column,
-            ns.label_table_entity_id_column
-        ) }}
-        {% endfor %}
+SELECT
+    {% for column in feature_columns %}
+    {{ column }},
+    {% endfor %}
+    lb.{{ label_name }},
+    lb.{{ label_table.entity_id_column }},
+    lb.{{ label_table.timestamp_column }}
+FROM
+    {{ ref(label_table_model) }} AS lb
+
+{% for group in features %}
+{{ stage_table_join(
+    "stg_" + group.feature_model_name + "_" + label_table_model,
+    "bike_id",
+    "date",
+    "lb",
+    label_table.entity_id_column,
+    label_table.timestamp_column
+) }}
+{% endfor %}
+
 {% endif %}
 {% endmacro %}
 
 {% macro stage_table_join(
-        stage_table_model_name,
-        stage_table_timestamp_column,
+        stage_table,
         stage_table_entity_id_column,
-        label_name,
-        label_table_timestamp,
-        label_table_entity_id
+        stage_table_timestamp_column,
+        label_table,
+        label_table_entity_id,
+        label_table_timestamp
     ) %}
-    INNER JOIN {{ ref(stage_table_model_name) }}
-    ON lb.{{ label_table_entity_id }} = {{ stage_table_model_name }}.{{ stage_table_entity_id_column }}
-    AND lb.{{ label_table_timestamp }} = {{ stage_table_model_name }}.{{ stage_table_timestamp_column }}
+INNER JOIN {{ ref(stage_table) }}
+    ON {{ label_table }}.{{ label_table_entity_id }} = {{ stage_table }}.{{ stage_table_entity_id_column }}
+    AND {{ label_table }}.{{ label_table_timestamp }} = {{ stage_table }}.{{ stage_table_timestamp_column }}
 {% endmacro %}
